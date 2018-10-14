ffce63bbd2dbf43f5c8188c3d5b12c36ac65793f
Beatriz Mendes <bea.smendes98@gmail.com>
2018-10-14 20:10:33 +0100

    fixed quad textures

--- primitives/MyQuad.js
+++ primitives/MyQuad.js
@@ -44,8 +44,8 @@ class MyQuad extends CGFobject
 			this.texCoords = [
 				0, 1, //this.minS, this.maxT,
 				1, 1, //this.maxS, this.maxT,
-				0, 0, //this.minS, this.minT,
-				1, 0, //this.maxS, this.minT
+				1, 0, //this.minS, this.minT,
+				0, 0, //this.maxS, this.minT
 			];
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
