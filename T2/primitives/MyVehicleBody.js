e93d873612795242f650326c569d138965679117
Maria Viana <mariaviana07@hotmail.com>
2018-11-24 20:27:23 +0000

    ovni coming into place

--- T2/primitives/MyVehicleBody.js
+++ T2/primitives/MyVehicleBody.js
@@ -21,9 +21,36 @@ class MyVehicleBody extends CGFobject
 		[1.5,0,-6],[1.5,0,6],
 		[-1.5,0,-6],[-1.5,0,6],
 		[-1.5,0,-1.5],[-1.5,0,1.5]];
-		this.sidePatch = new MyPatch(this.scene,50,50, 4, 2, sidePatchControlPoints);
-		this.topPatch = new MyPatch(this.scene,50,50, 4, 2, topPatchControlPoints);
+
+
+		// this.top = [
+		// 	[1, 1, 0],
+		// 	[0, 0, 0],
+		// 	[1, -1, 0], 
+
+		// 	[0, 0.5, 0],
+		// 	[0, 0, 2],
+		// 	[0, -0.5, 0],
+
+		// 	[-1, 1, 0],
+		// 	[0, 0, 0],
+		// 	[-1, -1, 0]
+		// ];
+
+		this.topPatch = new MyPatch(this.scene,9,9, 4, 2, topPatchControlPoints);
+		this.sidePatch = new MyPatch(this.scene,9,9, 4, 2, sidePatchControlPoints);	
 		this.sidePlane = new MyPlane(this.scene,50,50);
+		this.torus = new MyTorus(this.scene, 1, 2, 32, 20);
+
+
+		this.torusAppearence = new CGFappearance(this.scene);
+		this.torusAppearence.loadTexture("scenes/images/black.jpeg");
+
+		this.lightAppearence = new CGFappearance(this.scene);
+		this.lightAppearence.loadTexture("scenes/images/blue.jpg");
+
+		this.topAppearence = new CGFappearance(this.scene);
+		this.topAppearence.loadTexture("scenes/images/metal.jpg");
 	};
 
 	display(){
@@ -33,6 +60,7 @@ class MyVehicleBody extends CGFobject
 		this.scene.rotate(Math.PI/2,0,0,1);
 		this.scene.rotate(Math.PI/2,0,1,0);
 		this.scene.rotate(Math.PI/2,0,0,1);
+		this.topAppearence.apply();
 		this.sidePatch.display();
 		this.scene.popMatrix();
 
@@ -42,11 +70,13 @@ class MyVehicleBody extends CGFobject
 		this.scene.rotate(Math.PI/2,0,0,1);
 		this.scene.rotate(-Math.PI/2,0,1,0);
 		this.scene.rotate(Math.PI/2,0,0,1);
+		this.topAppearence.apply();
 		this.sidePatch.display();
 		this.scene.popMatrix();
 
 		this.scene.pushMatrix();
 		this.scene.scale(1,1,2);
+		this.topAppearence.apply();
 		this.topPatch.display();
 		this.scene.popMatrix();
 
@@ -54,6 +84,7 @@ class MyVehicleBody extends CGFobject
 		this.scene.translate(0,-0.4,3);
 		this.scene.scale(1,1,2);
 		this.scene.rotate(Math.PI,1,0,0);
+		this.topAppearence.apply();
 		this.topPatch.display();
 		this.scene.popMatrix();
 
@@ -62,6 +93,7 @@ class MyVehicleBody extends CGFobject
 		this.scene.scale(1,0.6,3);
 		this.scene.rotate(Math.PI/2,0,1,0);
 		this.scene.rotate(Math.PI/2,1,0,0);
+		this.topAppearence.apply();
 		this.sidePlane.display();
 		this.scene.popMatrix();
 
@@ -70,9 +102,25 @@ class MyVehicleBody extends CGFobject
 		this.scene.scale(1,0.6,3);
 		this.scene.rotate(-Math.PI/2,0,1,0);
 		this.scene.rotate(Math.PI/2,1,0,0);
+		this.topAppearence.apply();
 		this.sidePlane.display();
 		this.scene.popMatrix();
 
+		this.scene.pushMatrix();
+		this.scene.translate(0,-0.2,1.5);
+		this.scene.scale(0.9,0.3,0.9);
+		this.scene.rotate(Math.PI/2,1,0,0);
+		this.torusAppearence.apply();
+		this.torus.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.translate(0,-0.2,1.5);
+		this.scene.scale(0.95,0.1,0.95);
+		this.scene.rotate(Math.PI/2,1,0,0);
+		this.lightAppearence.apply();
+		this.torus.display();
+		this.scene.popMatrix();
 	};
 
 	updateTextCoords(length_s, length_t){
